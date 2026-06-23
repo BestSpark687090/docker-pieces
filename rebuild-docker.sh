7ed8f2fa09fde3c7961301ef5a663a0861a8232d
BestSpark687090 <bestspark687090@gmail.com>
2026-06-23 13:43:28 -0500

    add new studyreadies

--- rebuild-docker.sh
+++ rebuild-docker.sh
@@ -5,6 +5,7 @@ docker compose build --no-cache;
 docker compose up -d --force-recreate;
 # Surge rebuild because I can
 cd /home/www/BestSpark687090;
+#exit 0; # succeees but uhhh TODO: REMOVE LATER
 git pull;
 firebase deploy;
 surge /home/www/BestSpark687090 --domain bestspark.surge.sh
